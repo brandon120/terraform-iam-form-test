9b237cb7e6c55171e3999a66a40ef66732524be4
Agent Product Builder <deploy@agent-product-builder>
2026-06-26 18:16:54 +0000

    chore(builder): Workflow: brandon120/terraform-iam-form-test (main)

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,3 +1,11 @@
+resource "google_folder_iam_member" "group_roles" {
+  for_each = var.group_folder_roles
+
+  folder = var.folder_id
+  role   = each.value
+  member = "group:${var.group_email}"
+}
+
 resource "google_project_iam_member" "service_account_roles" {
   for_each = var.service_account_project_roles
 
